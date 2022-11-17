093a3963d342242334c20695db609de9ce93eb78
anteneh <112592567+tuna-11@users.noreply.github.com>
2022-11-16 22:07:43 -0700

    adding file

---
+++ employee1.java
@@ -0,0 +1,15 @@
+public class employee1 {
+    public static void (string[] args){
+        // TODO Auto-generated method stub
+        string name = "james Bond";
+        int salary = 2000;
+        double bouns = 200;
+        double total_salary = salary + bouns;
+
+        System.out.println("the name of employee is:" + name);
+        System.out.println("the salary of employee is:" + salary);
+        System.out.println("the bouns of employee is:" + bouns);
+        System.out.println("the total salary of employee is:" + total_salary);
+    }
+
+}
