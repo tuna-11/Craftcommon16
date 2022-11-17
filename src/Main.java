0f9f1eec1e81364d3fc7ca5d90bb000cdfe24229
anteneh <112592567+tuna-11@users.noreply.github.com>
2022-11-17 16:24:40 -0700

    adding one file

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String name = "james";
+        int myAge = 30;
+        double salary = 2000;
+        double bouns = 1000;
+        double total_salary = salary + bouns;
+        System.out.println("the name of the student" + name);
+        System.out.println("the age of the student" + myAge);
+        System.out.println("the salary of the student" + salary);
+        System.out.println("the bouns of the student" + bouns);
+        System.out.println("the total_salary of student" + salary + bouns);
     }
 }
